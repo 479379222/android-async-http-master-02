6aa784be894e639bfed4dd0937cf91593cb55971
479379222 <479379222@qq.com>
2015-12-17 16:46:49 +0800

    listtest

--- myasyntest/src/main/java/com/example/myas/myasyntest/BaseserviceActivity.java
+++ myasyntest/src/main/java/com/example/myas/myasyntest/BaseserviceActivity.java
@@ -2,12 +2,21 @@ package com.example.myas.myasyntest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class BaseserviceActivity extends AppCompatActivity {
 
+    private ListView lstView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baseservice);
+        initView();
+
     }
+
+    private void initView(){
+        lstView=(ListView)findViewById(R.id.lstView);
+    }
+
 }
